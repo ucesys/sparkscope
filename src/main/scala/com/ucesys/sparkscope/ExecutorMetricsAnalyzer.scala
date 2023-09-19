3a6cbd6b3522dad0487d279bc95e7e1dcef7688f
psobczak <piotr.sobczak@ucesys.com>
2023-09-20 01:02:28 +0200

    Add converting ts do dt

--- src/main/scala/com/ucesys/sparkscope/ExecutorMetricsAnalyzer.scala
+++ src/main/scala/com/ucesys/sparkscope/ExecutorMetricsAnalyzer.scala
@@ -23,7 +23,8 @@ import com.ucesys.sparkscope.io.{CsvHadoopReader, CsvReader, PropertiesLoader}
 import org.apache.spark.SparkConf
 
 import java.io.FileWriter
-import java.nio.file.{Files, Paths}
+import java.time.LocalDateTime.ofEpochSecond
+import java.time.ZoneOffset.UTC
 import scala.collection.mutable
 
 class ExecutorMetricsAnalyzer(sparkConf: SparkConf, reader: CsvReader, propertiesLoader: PropertiesLoader) extends  AppAnalyzer {
@@ -201,45 +202,12 @@ class ExecutorMetricsAnalyzer(sparkConf: SparkConf, reader: CsvReader, propertie
     out.println(s"Max non-heap memory utilization by driver: ${maxNonHeapUsedDriver}MB")
     out.println(f"Average non-heap memory utilization by driver: ${avgNonHeapUsedDriver}MB")
 
-//    val vals = Array(0.71477137, 0.55749811, 0.50809619, 0.47027228, 0.25281568, 0.66633175, 0.50676332, 0.6007552, 0.56892904, 0.49553407, 0.61093935, 0.65057417, 0.40095626, 0.45969447, 0.51087888, 0.52894806, 0.49397198, 0.4267163, 0.54091298, 0.34545257, 0.58548892, 0.3137885, 0.63521146, 0.57541744, 0.59862265, 0.66261386, 0.56744017, 0.42548488, 0.40841345, 0.47393027, 0.60882106, 0.45961208, 0.43371424, 0.40876484, 0.64367337, 0.54092033, 0.34240811, 0.44048106, 0.48874236, 0.68300902, 0.33563968, 0.58328107, 0.58054283, 0.64710522, 0.37801285, 0.36748982, 0.44386445, 0.47245989, 0.297599, 0.50295541, 0.39785732, 0.51370486, 0.46650358, 0.5623638, 0.4446957, 0.52949791, 0.54611411, 0.41020067, 0.61644868, 0.47493691, 0.50611458, 0.42518211, 0.45467712, 0.52438467, 0.724529, 0.59749142, 0.45940223, 0.53099928, 0.65159718, 0.38038268, 0.51639554, 0.41847437, 0.46022878, 0.57326103, 0.44913632, 0.61043611, 0.42694949, 0.43997814, 0.58787928, 0.36252603, 0.50937634, 0.47444256, 0.57992527, 0.29381335, 0.50357977, 0.42469464, 0.53049697, 0.7163579, 0.39741694, 0.41980533, 0.68091159, 0.69330702, 0.50518926, 0.55884098, 0.48618324, 0.48469854, 0.55342267, 0.67159111, 0.62352006, 0.34773486)
-
-
-//    val dataset = new HistogramDataset
-//    dataset.addSeries("key", vals, 50)
-
-//    val histogram = ChartFactory.createHistogram("Normal distribution", "y values", "x values", dataset, PlotOrientation.HORIZONTAL, false, false, false)
-
-//    ChartUtilities.saveChartAsPNG(new File("histogram.png"), histogram, 450, 400)
-
-//    var series = new XYSeries("2016");
-//    series.add(18, 567);
-//    series.add(20, 612);
-//    series.add(25, 800);
-//    series.
-//    var dataset = new XYSeriesCollection();
-//    dataset.addSeries(series);
-//
-//    val chart = ChartFactory.createXYLineChart(
-//      "Average salary per age",
-//      "Age",
-//      "Salary (€)",
-//      dataset,
-//      PlotOrientation.VERTICAL,
-//      true,
-//      true,
-//      false
-//    );
-//        ChartUtilities.saveChartAsPNG(new File("histogram.png"), chart, 450, 400)
-
-
     val endTimeMillis = System.currentTimeMillis()
     val durationSeconds = (endTimeMillis - startTimeMillis)*1f / 1000f
     out.println(s"\n[SparkScope] SparkScope analysis took ${durationSeconds}s")
 
     val html = out.toString.replace("\n", "<br>\n")
     val fileWriter = new FileWriter("report.html")
-
-//    val path = Paths.get("report.html.template")
     val template = scala.io.Source.fromFile("report.html.template").mkString
 
     val rendered = template
@@ -247,8 +215,9 @@ class ExecutorMetricsAnalyzer(sparkConf: SparkConf, reader: CsvReader, propertie
       .replace("<REPORT-BODY>", html)
       .replace("${chart.jvm.cluster.heap.current}", clusterHeapUsed.select("jvm.heap.used").div(1024*1024).mkString(","))
       .replace("${chart.jvm.cluster.heap.max}", clusterHeapMax.select("jvm.heap.max").div(1024*1024).mkString(","))
-      .replace("${chart.jvm.cluster.heap.timestamps}", clusterHeapMax.select("t").values.mkString(","))
-
+      .replace(
+        "${chart.jvm.cluster.heap.timestamps}",
+        clusterHeapMax.select("t").values.map(ts => s"'${ofEpochSecond(ts.toLong, 0, UTC)}'").mkString(","))
     fileWriter.write(rendered)
     fileWriter.close()
 
