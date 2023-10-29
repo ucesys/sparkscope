195789d907488c74640eec4c079912eba49af8fd
psobczak <piotr.sobczak@ucesys.com>
2023-10-29 18:54:19 +0100

    Add log informing that there's no executor remove time

--- src/main/scala/com/ucesys/sparkscope/common/ExecutorContext.scala
+++ src/main/scala/com/ucesys/sparkscope/common/ExecutorContext.scala
@@ -6,7 +6,15 @@ case class ExecutorContext(executorId: String,
                            cores: Long,
                            addTime: Long,
                            removeTime: Option[Long]) {
-    def upTime(lastMetricTimeMs: Long): Long = removeTime.getOrElse(lastMetricTimeMs*1000) - addTime
+    def upTime(lastMetricTimeMs: Long)(implicit logger: SparkScopeLogger): Long = {
+        val executorEndTime: Long = removeTime match {
+            case Some(time) => time
+            case None =>
+                logger.info(s"Missing remove time for executorId=${executorId}, using last metric timestamp to calculate uptime")
+                lastMetricTimeMs*1000
+        }
+        executorEndTime - addTime
+    }
 }
 
 object ExecutorContext {
