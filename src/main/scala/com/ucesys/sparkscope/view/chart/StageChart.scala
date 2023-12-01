bc7aaa98dc926dba515323fac411518c8be188f6
psobczak <piotr.sobczak@ucesys.com>
2023-12-01 16:57:42 +0100

    Add stage chart limitation

--- src/main/scala/com/ucesys/sparkscope/view/chart/StageChart.scala
+++ src/main/scala/com/ucesys/sparkscope/view/chart/StageChart.scala
@@ -3,7 +3,7 @@ package com.ucesys.sparkscope.view.chart
 import com.ucesys.sparkscope.common.MetricUtils.ColTs
 import com.ucesys.sparkscope.common.SparkScopeLogger
 import com.ucesys.sparkscope.data.{DataColumn, DataTable}
-import com.ucesys.sparkscope.view.HtmlReportGenerator.MaxChartPoints
+import com.ucesys.sparkscope.view.HtmlReportGenerator.MaxStageChartPoints
 import com.ucesys.sparkscope.view.SeriesColor
 import com.ucesys.sparkscope.view.SeriesColor._
 
@@ -12,33 +12,41 @@ case class StageChart(timestamps: Seq[String], datasets: String) extends Chart {
 }
 
 object StageChart {
-    case class TimePoint(ts: String, value: String, limit: String)
+    case class TimePoint(ts: String, value: String)
 
     def apply(stageTimeline: DataTable)(implicit logger: SparkScopeLogger): StageChart = {
+        val tsCol = stageTimeline.select(ColTs)
         val stageCols = stageTimeline.columns.filter(_.name != ColTs)
-        val datasetsStr = generateStages(stageCols)
-        StageChart(stageTimeline.select(ColTs).values, datasetsStr)
-//        if(tsCol.size != valueCol.size) {
-//            throw new IllegalArgumentException(s"Series sizes are different: ${tsCol.size} and ${valueCol.size}")
-//        } else if (tsCol.size <= MaxChartPoints) {
-//            StageChart(tsCol.values, valueCol.values)
-//        } else {
-//            logger.info(s"Limiting chart points from ${tsCol.size} to ${MaxChartPoints}")
-//            val ratio: Float = tsCol.size.toFloat / MaxChartPoints.toFloat
-//            val newPoints: Seq[Int] = (0 until MaxChartPoints)
-//            val newChart: Seq[TimePoint] = newPoints.map{ id =>
-//                val from: Int = (id*ratio).toInt
-//                val to: Int = (from + ratio).toInt
-//                val values = valueCol.values.slice(from, to).map(_.toDouble.toLong)
-//                val avg = values.sum/values.length
-//                TimePoint(tsCol.values(from), avg.toString)
-//            }
-//            StageChart(newChart.map(_.ts), newChart.map(_.value))
-//        }
-    }
 
-    def generateStages(stageCols: Seq[DataColumn]): String = {
-        stageCols.map(generateStageData).mkString("[", ",", "]")
+        if(stageCols.exists(_.size != tsCol.size)) {
+            throw new IllegalArgumentException(s"Stage series sizes are different: ${Seq(tsCol.size) ++ stageCols.map(_.size)}")
+        } else if (stageCols.map(_.size).sum <= MaxStageChartPoints) {
+            StageChart(tsCol.values, stageCols.map(generateStageData).mkString("[", ",", "]"))
+        } else {
+            logger.info(s"Limiting total stage chart points from ${stageCols.map(_.size).sum} to ${MaxStageChartPoints}")
+            val singleChartPoints: Int = MaxStageChartPoints / stageCols.length
+            logger.info(s"Limiting every stage chart points from ${stageCols.headOption.map(_.size).getOrElse(0)} to ${singleChartPoints}")
+
+            val ratio: Float = tsCol.size.toFloat / singleChartPoints.toFloat
+            val newPoints: Seq[Int] = (0 until singleChartPoints)
+
+            val newTimestamps: Seq[String] = newPoints.map(id => tsCol.values((id * ratio).toInt))
+
+            val newStageCols: Seq[DataColumn] = stageCols.map{ stageCol =>
+                val values: Seq[String] = newPoints.map { id =>
+                    val from: Int = (id * ratio).toInt
+                    val to: Int = (from + ratio).toInt
+                    val valuesNotNull = stageCol.values.slice(from, to).filter(_ != "null")
+
+                    valuesNotNull match {
+                        case Seq() => "null"
+                        case _ => valuesNotNull.map(_.toDouble.toLong).max.toString
+                    }
+                }
+                DataColumn(stageCol.name, values)
+            }
+            StageChart(newTimestamps, newStageCols.map(generateStageData).mkString("[", ",", "]"))
+        }
     }
 
     def generateStageData(stageCol: DataColumn): String = {
