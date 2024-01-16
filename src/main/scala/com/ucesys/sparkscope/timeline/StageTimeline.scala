361c7ad52e26f6725ebb84ed7c0704738b836805
psobczak <piotr.sobczak@ucesys.com>
2024-01-16 13:39:45 +0100

    Apply Apache 2.0 License

--- src/main/scala/com/ucesys/sparkscope/timeline/StageTimeline.scala
+++ src/main/scala/com/ucesys/sparkscope/timeline/StageTimeline.scala
@@ -14,6 +14,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
+
 package com.ucesys.sparkscope.timeline
 
 import org.apache.spark.scheduler.{SparkListenerStageCompleted, SparkListenerStageSubmitted}
